2d7cf3f92b5bdb00c013a93b1b8b318b3e420df9
SophieMcFarlane <sophie.e.mcfarlane@durham.ac.uk>
2020-11-15 14:28:38 +0000

    Linked to Database

--- app.js
+++ app.js
@@ -2,8 +2,26 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 
+//Loading in the Database
+let db = new sqlite3.Database('./db.sqlite', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database');
+});
+
+//Closing the Database
+db.close( (err) => {
+    if(err){
+        return console.error(err.message);
+    }
+    console.log('Closed the database connection');
+});
+
+//Exporting the module
 module.exports = app;
