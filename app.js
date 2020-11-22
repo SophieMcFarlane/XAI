ce8272419f74bede2d89bf227c2db9fca5289489
SophieMcFarlane <sophie.e.mcfarlane@durham.ac.uk>
2020-11-22 16:17:08 +0000

    changed backend of database

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ let db = new sqlite3.Database('./db.sqlite', (err) => {
     console.log('Connected to the in-memory SQlite database');
 });
 
+//Querying the Database
+db.serialize(() => {
+    db.each(`SELECT movieId as id, title as name FROM cleaned_movies`, (err, row) => {
+        if(err){
+            console.log(err.message);
+        }
+        console.log(row.id + "\t" + row.name);
+    });
+
+})
+
 //Closing the Database
 db.close( (err) => {
     if(err){
