fc7472773adfd63415933c4172174a420ce16b2a
SophieMcFarlane <sophie.e.mcfarlane@durham.ac.uk>
2021-01-08 15:22:52 +0000

    Finished the getRecommendations get request

--- app.js
+++ app.js
@@ -105,28 +105,81 @@ app.get('/getRecommendations', function(req, res){
     var timeg3 = 0;
     var timeg4 = 0;
     var timeg5 = 0;
+    var group1Rating = 0;
+    var group2Rating = 0;
+    var group3Rating = 0;
+    var group4Rating = 0;
+    var group5Rating = 0;
+    var userRatings = [];
+    //Gets called for each row
     db.each('SELECT * FROM user_behaviour ORDER BY timestamp DESC', (error, result) => {
+        //Regex expressions to match the buttons
         var reg1 = /g1/;
         var reg2 = /g2/;
         var reg3 = /g3/;
         var reg4 = /g4/;
         var reg5 = /g5/;
+        //Find the latest button from g1 movies that has been clicked
         if (result.userId === username && result.timestamp > timeg1 && reg1.test(result.button)){
             timeg1 = result.timestamp;
+            group1Rating = result.button.charAt(5);
         };
+        //Find the latest button from g2 movies that has been clicked
         if (result.userId === username && result.timestamp > timeg2 && reg2.test(result.button)){
             timeg2 = result.timestamp;
+            group2Rating = result.button.charAt(5);
         };
+        //Find the latest button from g3 movies that has been clicked
         if (result.userId === username && result.timestamp > timeg3 && reg3.test(result.button)){
             timeg3 = result.timestamp;
+            group3Rating = result.button.charAt(5);
         };
+        //Find the latest button from g4 movies that has been clicked
         if (result.userId === username && result.timestamp > timeg4 && reg4.test(result.button)){
             timeg4 = result.timestamp;
+            group4Rating = result.button.charAt(5);
         };
+        //Find the latest button from g5 movies that has been clicked
         if (result.userId === username && result.timestamp > timeg5 && reg5.test(result.button)){
             timeg5 = result.timestamp;
+            group5Rating = result.button.charAt(5);
         };
-        //res.send(result);
+        //Creates a list of all the ratings for each group
+        userRatings = [group1Rating, group2Rating, group3Rating, group4Rating, group5Rating];
+
+    }, (error, numberofRows) => {
+        //Only gets called onced after all the above rows have been checked
+        var highestRated = 0;
+        var highestRatedIndex = 0;
+        //Find the highest rated group
+        for (var i=0; i<5; i++){
+            if(userRatings[i] > highestRated){
+                highestRated = userRatings[i];
+                highestRatedIndex = i;
+            }
+        }
+        //Send the cluster that was rated highest
+        if(highestRatedIndex == 0){
+            db.all('SELECT * FROM cluster1_edited ORDER BY rank DESC', (error, result) => {
+                res.send(result);
+            });
+        }else if(highestRatedIndex == 1){
+            db.all('SELECT * FROM cluster2_edited ORDER BY rank DESC', (error, result) => {
+                res.send(result);
+            });
+        }else if(highestRatedIndex == 2){
+            db.all('SELECT * FROM cluster3_edited ORDER BY rank DESC', (error, result) => {
+                res.send(result);
+            });
+        }else if(highestRatedIndex == 3){
+            db.all('SELECT * FROM cluster4_edited ORDER BY rank DESC', (error, result) => {
+                res.send(result);
+            });
+        }else if(highestRatedIndex == 4){
+            db.all('SELECT * FROM cluster5_edited ORDER BY rank DESC', (error, result) => {
+                res.send(result);
+            });
+        }
     })
 })
 
